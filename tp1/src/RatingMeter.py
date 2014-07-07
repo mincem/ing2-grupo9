dab276e77b4b4114a98e81d21d1331c9064eb64b
sansen <santi73@gmail.com>
2014-07-07 17:33:58 -0300

    2.cambios para trabajar offline y online segun boton. (no testeado)

--- tp1/src/RatingMeter.py
+++ tp1/src/RatingMeter.py
@@ -3,8 +3,9 @@ from PostProvider import *
 
 class RatingMeter(Meter):
     """Class to measure the rating"""
-    def __init__(self, tvShow, aDate):
+    def __init__(self, tvShow, aDate, decision):
         self._date = aDate
+        self._decision = decision
         super(RatingMeter, self).__init__(tvShow)
 
     def getDate(self):
@@ -12,7 +13,7 @@ class RatingMeter(Meter):
 
     def measure(self):
         if self._qPosts == []:
-            aPostProvider = PostProvider()
+            aPostProvider = PostProvider(self._decision)
             self._qPosts = aPostProvider.qualifiedPostsDuringAirtime(self._tvShow,
                                                                      self._date)
         self.notify(self._qPosts)
